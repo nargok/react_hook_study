15576e351fedbc2d3c15a0184074720764959d4c
nargok <nargok@gmail.com>
2019-08-10 23:32:28 +0900

    create event with useReducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import reducer from '../reducers/index';
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    setTitle('')
+    setBody('')
+  }
+
+  console.log(state)
+
   return (
     <div className="container-fruid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
 
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)} />
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">すべてのイベントを削除する</button>
       </form>
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ const events = (state = [], action) => {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body }
       const length = state.length
-      const id = length === 0 ? id = 1 : state[length - 1].id + 1
+      const id = length === 0 ? 1 : state[length - 1].id + 1
       return [...state, {id, ...event}]
     case 'DELETE_EVENT':
     case 'DELETE_ALL_EVENTS':
