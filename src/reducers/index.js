c46635bad0fb532deca2f1265bd29781f22c958b
nargok <nargok@gmail.com>
2019-08-10 23:14:32 +0900

    implement reducer for create events

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const events = (state = [], action) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body }
+      const length = state.length
+      const id = length === 0 ? id = 1 : state[length - 1].id + 1
+      return [...state, {id, ...event}]
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENTS':
+    default:
+      return state
+  }
+}
+
+export default events
