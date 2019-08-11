749dc42b8b561e42ab4b6353be5f935a301481c7
nargok <nargok@gmail.com>
2019-08-11 15:45:40 +0900

    implement delete all events

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,11 @@ const App = () => {
     setBody('')
   }
 
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    dispatch({ type: 'DELETE_ALL_EVENTS'})
+  }
+
   console.log(state)
 
   return (
@@ -40,7 +45,7 @@ const App = () => {
         </div>
 
         <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">すべてのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents}>すべてのイベントを削除する</button>
       </form>
 
       <h4>イベント一覧</h4>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,6 +8,7 @@ const events = (state = [], action) => {
     case 'DELETE_EVENT':
       return state.filter(event => event.id !== action.id)
     case 'DELETE_ALL_EVENTS':
+      return []
     default:
       return state
   }
